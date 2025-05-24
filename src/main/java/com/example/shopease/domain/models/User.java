dadd0486c56e10f4a59e86a3d6e14e056b0d0a01
knah1d <bsse1437@iit.du.ac.bd>
2025-05-24 22:50:39 +0600

    Initialize ShopEase project with Maven, Spring Boot, and PostgreSQL configuration

---
+++ src/main/java/com/example/shopease/domain/models/User.java
@@ -0,0 +1,12 @@
+package com.ecommerce.domain.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String role; // "CUSTOMER" or "SELLER"
+}
