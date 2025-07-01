d4e1ef5d28cde5da781ea73b897c9d415954c3ae
knah1d <bsse1437@iit.du.ac.bd>
2025-07-01 18:28:47 +0600

    Remove unused User-related classes and JpaUserRepository to streamline the codebase

--- src/main/java/com/example/shopease/domain/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.shopease.domain.repositories;
-
-import com.example.shopease.domain.models.User;
-
-public interface UserRepository {
-    User save(User user);
-    User findByEmail(String email);
-}
